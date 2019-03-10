fdcb43b4abc9c4a6ee1715f6de52e6189791fc75
Tyler Paige <tylerclarkpaige@gmail.com>
2019-03-10 16:42:59 -0400

    Optimizes images & production build process

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,6 @@ const merge = require('webpack-merge');
 const base = require('./webpack.config.js');
 
 module.exports = merge(base, {
-  mode : 'production'
-})
+  mode : 'production',
+  devtool: 'none'
+});
